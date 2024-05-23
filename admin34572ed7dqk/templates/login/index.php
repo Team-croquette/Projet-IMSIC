25622f53945a1c47e42002760453dff00874335a
Thibaut LAHAIE <thibaut.lahaie@etu.univ-amu.fr>
2024-05-23 09:41:55 +0200

    ajout du style pour la page de connexion admin

--- admin34572ed7dqk/templates/login/index.php
+++ admin34572ed7dqk/templates/login/index.php
@@ -1,31 +1,39 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="UTF-8">
+    <title>Accès Espace Administrateur</title>
+    <link rel="stylesheet" href="<?= $adminRoot; ?>/assets/css/style.css">
 
-</html>
-
+</head>
 <body>
-    <div class="content">
-        <ul class="alert-error">
-            <?php
-            foreach ($errors as $message) {
+
+<header>
+    HEADER
+</header>
+<div class="content">
+    <ul class="alert-error">
+        <?php
+        foreach ($errors as $message) {
             ?>
-                <li>
-                    <?= $message; ?>
-                </li>
+            <li>
+                <?= $message; ?>
+            </li>
             <?php
-            }
-            ?>
-        </ul>
-        <div>
-            <div>
-                <?= $form; ?>
-            </div>
-            <div>
-                <img src="<?= $adminRoot; ?>/assets/img/white-star.png"/>
-                <h1>Accès Espace Administrateur</h1>
-                <img src="<?= $adminRoot; ?>/assets/img/black-star.png"/>
-            </div>
+        }
+        ?>
+    </ul>
+    <div id="adminConnect">
+        <div id="form">
+            <?= $form; ?>
+        </div>
+        <div id="text">
+            <img class="left-star" src="<?= $adminRoot; ?>/assets/img/white-star.png"/>
+            <h1>Accès Espace<br/>Administrateur</h1>
+            <img class="right-star" src="<?= $adminRoot; ?>/assets/img/black-star.png"/>
         </div>
     </div>
+</div>
 
 </body>
+</html>
