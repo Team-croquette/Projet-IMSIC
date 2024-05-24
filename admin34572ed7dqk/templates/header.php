86e60916daf53ada95382c5580c64f1bdd3ca612
Thibaut LAHAIE <thibaut.lahaie@etu.univ-amu.fr>
2024-05-24 12:17:21 +0200

    style of the header

--- admin34572ed7dqk/templates/header.php
+++ admin34572ed7dqk/templates/header.php
@@ -1,10 +1,17 @@
 <header>
-    <ul>
-        <li>
-            <a>Questionnaire</a>
-        </li>
-        <li>
-            <a href="<?= $adminRoot ?>/disconnect">Déconnexion</a>
-        </li>
-    </ul>
+    <div id="left-part">
+        <img class="left-star" src="<?= $adminRoot; ?>/assets/img/ask-question.png"/>
+        <a>IMSIC Questionnaire</a>
+    </div>
+
+    <div id="right-part">
+        <?php
+        if (key_exists('token', $_SESSION)) {
+            ?>
+            <a href="<?= $adminRoot ?>/disconnect" id="disconnect">Déconnexion</a>
+            <?php
+        }
+        ?>
+        <a id="question"><img class="left-star" src="<?= $adminRoot; ?>/assets/img/Help.png"/>Question</a>
+    </div>
 </header>
