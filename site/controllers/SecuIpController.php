e8808c713472188b32d0ce62e9f2873378853831
Phyz <nathan.vigon@etu.univ-amu.fr>
2024-05-27 16:12:13 +0200

    Modification de la gestion de securité par ip

--- site/controllers/SecuIpController.php
+++ site/controllers/SecuIpController.php
@@ -18,69 +18,65 @@ class SecuIpController extends ControllerCore
 
     public function run() : bool
     {
-        $ipRouter = $this->getIp()[1];
-        $ipClient = $this->getIp()[0];
-        $resultIpFromRouter = $this->toManyIpFromRouter($ipRouter);
-        var_dump($resultIpFromRouter['case']);
-        if ($this->ipUsed($ipRouter, $ipClient)) {
-            $this->info = 'Ip déjà utilisée';
-            return true;
-        } elseif ($resultIpFromRouter['case'] < 2) {
-            $secu = new \SecuIpModel();
-            $secu->addIp($ipRouter, $ipClient);
-            $this->info = $resultIpFromRouter['info'];
+        //TODO: Deplacer les addIp dans le questionnaireController (ou après clique du liens) quand la page sera faite
+        $ip = $this->getIp();
+        //var_dump($coFromIp['case']);
+        $secu = new \SecuIpModel();
+        if ( ! $this->ipUsed($ip)) {
+            $this->info = 'Début du questionnaire';
+            $secu->addIp($ip);
             return false;
-        } elseif ($resultIpFromRouter['case']==2) {
-            $this->info = $resultIpFromRouter['info'];
-            return true;
         } else {
-            $secu = new \SecuIpModel();
-            $secu->addIp($ipRouter, $ipClient);
-            $this->info = $resultIpFromRouter['info'];
-            return false;
+            $coFromIp = $this->toManyCoFromIp($ip);
+
+            if ($coFromIp['case'] < 2) {
+                $secu->addIp($ip);
+                $this->info = $coFromIp['info'];
+                return false;
+            } elseif ($coFromIp['case'] == 2) {
+                $this->info = $coFromIp['info'];
+                return true;
+            } else {
+                $secu = new \SecuIpModel();
+                $secu->addIp($ip);
+                $this->info = $coFromIp['info'];
+                return false;
+            }
         }
-        die;
     }
 
-    private function ipUsed($ipRouter, $ipClient): bool
+    private function ipUsed($ip): bool
     {
         $secu = new \SecuIpModel();
-        return $secu->ipUsed($ipRouter, $ipClient);
+        return $secu->ipUsed($ip);
     }
 
-    private function toManyIpFromRouter($ipRouter) {
+    private function toManyCoFromIp($ip) {
         $secu = new \SecuIpModel();
-        $ipInRouter = $secu->countIpInServ($ipRouter);
-        if ($ipInRouter<5) {
-            return ["case"=> 0, "info" => $ipInRouter . " appareil(s) a(ont) déjà éssayé de se connecté depuis votre réseaux. \n" . 5 - $ipInRouter . 'connexions restante(s).'];
-        } elseif ($ipInRouter == 5) {
-            return ["case"=> 1, "info" => $ipInRouter . " appareils ont déjà éssayé de se connecté depuis votre réseaux. \n Ceci sera votre dernière tentative."];
+        $nbrCoFromIp = $secu->nbrCoFromIp($ip);
+        if ($nbrCoFromIp<4) {
+            return ["case"=> 0, "info" => $nbrCoFromIp . " appareil(s) a(ont) déjà éssayé de se connecté depuis votre réseaux. \n" . 5 - $nbrCoFromIp . 'connexions restante(s).'];
+        } elseif ($nbrCoFromIp == 4) {
+            return ["case"=> 1, "info" => $nbrCoFromIp . " appareils ont déjà éssayé de se connecté depuis votre réseaux. \n Ceci sera votre dernière tentative."];
         } else {
             return ["case"=> 2, "info" => 'Nous avons déjà recu le nombres de tentatives maximum pour ce questionnaire depuis votre réseaux.'];
         }
     }
 
-    private function getIp()
+    public function getIp()
     {
-        $ips = [];
-
         //recupération des ips du client
         if ( isset( $_SERVER['HTTP_X_REAL_IP'] ) ) {
-            $ips[0] = $_SERVER['HTTP_X_REAL_IP'];
+            $ip = $_SERVER['HTTP_X_REAL_IP'];
         } else if (isset($_SERVER['HTTP_CLIENT_IP'])){
-            $ips[0] = $_SERVER['HTTP_CLIENT_IP'];
-        } else {
-            $ips[0] = "Was not able to get client ip";
-        }
-
-        //récuperation des ips (potentielement du routeur ou parfois de l'hebergeur du site selon les cas)
-        if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-            $ips[1] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } else if ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else if ( isset( $_SERVER['REMOTE_ADDR'] ) ) {
-            $ips[1] = $_SERVER['REMOTE_ADDR'];
+            $ip = $_SERVER['REMOTE_ADDR'];
         } else {
-            $ips[1] = "Was not able to get router ip";
+            $ip = "Was not able to get client ip";
         }
-        return $ips;
+        return $ip;
     }
 }
