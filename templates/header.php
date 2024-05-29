f30d04d79bea59e0e9f4705acf2cfede50395a7c
Arthur Lecomte <arthur.lecomte@etu.univ-amu.fr>
2024-05-29 11:22:26 +0200

    implémentation complete du captcha

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,19 @@
 <header>
-<link rel="stylesheet" href="<?= $siteRoot ?>/assets/style/popupCaptchaStyle.css">
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<script src="<?= $siteRoot ?>/assets/script/scriptCaptcha.js"></script>
+
+    <!-- code captcha -->
+    <link rel="stylesheet" href="<?= $siteRoot ?>/assets/style/popupCaptchaStyle.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script type="text/javascript">
+        const siteRoot = '<?= $siteRoot;?>';
+    </script>
+    <script src="<?= $siteRoot ?>/assets/script/scriptCaptcha.js"></script>
+    <?php include 'templates/popupCaptcha/index.php' ?>
+
     <nav>
         <ul>
             <li><a href="home">Accueil</a></li>
             <li><a href="admin">Admin</a></li>
-            <li><a href="quest" <?= $resultSecuIp; ?> >Questionnaire</a></li>
+            <li><a id="openPopupBtn" <?= $resultSecuIp; ?> >Questionnaire</a></li>
         </ul>
     </nav>
 </header>
