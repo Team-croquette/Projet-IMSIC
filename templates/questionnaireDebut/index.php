1a99aa7b566444da172226121c5ea315c88f7303
Marion Valls <marion.valls@etu.univ-amu.fr>
2024-06-04 13:59:08 +0200

    feat : page debut questionnaire

--- templates/questionnaireDebut/index.php
+++ templates/questionnaireDebut/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Fin du questionnaire</title>
+    <title>Début du questionnaire</title>
     <link rel="stylesheet" href="<?= $siteRoot; ?>/assets/css/style.css">
     <link rel="icon" type="image/x-icon" href="../assets/img/icon_question_mark.ico">
 </head>
@@ -13,12 +13,13 @@
     <!-- Sub Header -->
     <div class="sub-header">
         <div class="left-part">
-            <span class="title">Merci d'avoir répondu au <br>questionnaire</span>
-            <p>Pour nous aider à évaluer et comparer les perceptions de la valeur de l'information entre les
-                journalistes/professionnels de l'information et le grand public</p>
+            <span class="title">Avant de répondre au <br>questionnaire</span>
+            <p>Merci de prendre connaissances des conditions générales d'utilisation ci-dessous.<br>
+                Les résultats sont anonymes et ne éseront pas utilisés à des fins commerciales.
+            </p>
 
             <div id="home-buttons">
-                <a id="home-imsic" class="button" href="https://www.imsic.fr/">L’IMSIC c’est quoi ?</a>
+                <a id="home-imsic" class="button" href="../CGU.pdf">Lire les conditions</a>
             </div>
         </div>
         <div class="right-part">
@@ -37,6 +38,13 @@
     </div>
     <!-- End Sub Header -->
 
+    <!-- Home content -->
+    <div class="home-content">
+        <div id="form">
+            <?= $form; ?>
+        </div>
+        </div>
+    </div>
 </div>
 <div id="void">
 </div>
