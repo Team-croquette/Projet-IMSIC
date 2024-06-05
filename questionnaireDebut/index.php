27ae38e934465294d2a5625e5c579275618e899d
Marion Valls <marion.valls@etu.univ-amu.fr>
2024-06-05 16:24:48 +0200

    fix

--- questionnaireDebut/index.php
+++ questionnaireDebut/index.php
@@ -5,10 +5,12 @@ session_start();
 if (isset($_SESSION["VerifCapcha"])){
     if (!$_SESSION["VerifCapcha"]){
         header("Location: ../index.php");
+        die;
     }
 }
 else{
     header("Location: ../index.php");
+    die;
 }
 
 foreach(glob('../classes/*.php') as $fileName){
@@ -19,7 +21,7 @@ foreach(glob('../classes/*.php') as $fileName){
 }
 foreach(glob(ADMIN_PATH.'/classes/*.php') as $fileName){
 
-    if (!str_contains($fileName,'index.php') && !str_contains($fileName,'AdminController')) {
+    if (!str_contains($fileName,'index.php') && !str_contains($fileName,'AdminController') && !str_contains($fileName,'ModelCore.php')) {
         require_once $fileName;
     }
 }
